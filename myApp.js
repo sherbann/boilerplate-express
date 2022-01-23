1fc0672f2f8a4d009e26473721b958042ed4231e
sherbann <93603838+sherbann@users.noreply.github.com>
2022-01-23 17:23:40 +0000

    html

--- myApp.js
+++ myApp.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-
+/**
+ * app.get('/', function (req, res) {
+   res.send("Hello Express");
+});
+ */
 
 
 
